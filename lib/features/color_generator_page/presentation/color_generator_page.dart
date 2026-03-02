d6af0fd6badbf42b81bcf028c27ef574f7a9a489
Viktoriia Cherkashyna <cherk.viktoriia@gmail.com>
2026-03-02 09:37:27 +0100

    fix: Update history list by listener;

--- lib/features/color_generator_page/presentation/color_generator_page.dart
+++ lib/features/color_generator_page/presentation/color_generator_page.dart
@@ -99,9 +99,7 @@ class _ColorGeneratorPageState extends State<ColorGeneratorPage> {
     final parsedValue = roundToDecimals(opacity);
     final newColor = getNewRgbaColor(opacity: parsedValue);
 
-    _clrBloc
-      ..add(ColorGeneratorEvent.setBackgroundColor(color: newColor))
-      ..add(ColorGeneratorEvent.setColorsHistory(color: newColor));
+    _clrBloc.add(ColorGeneratorEvent.setBackgroundColor(color: newColor));
   }
 
   void _updateOpacity(double value) {
@@ -112,9 +110,7 @@ class _ColorGeneratorPageState extends State<ColorGeneratorPage> {
       clr: _clrBloc.state.data.bgColor,
     );
 
-    _clrBloc
-      ..add(ColorGeneratorEvent.setBackgroundColor(color: updatedColor))
-      ..add(ColorGeneratorEvent.setColorsHistory(color: updatedColor));
+    _clrBloc.add(ColorGeneratorEvent.setBackgroundColor(color: updatedColor));
   }
 
   void _resetColor() => _clrBloc.add(
@@ -127,78 +123,91 @@ class _ColorGeneratorPageState extends State<ColorGeneratorPage> {
     });
   }
 
+  void _updateHistory(Color newColor) =>
+      _clrBloc.add(ColorGeneratorEvent.setColorsHistory(color: newColor));
+
   @override
   Widget build(BuildContext context) {
     final showcaseKeys = [_firstKey, _secondKey, _thirdKey, _fourthKey];
 
-    return ToolTips(
-      onComplete: (_, __) => _onCompleteGuideTour(),
-      onDismiss: (_) => _onCompleteGuideTour(),
-      autoStart: _tooltipsAutoStart,
-      showcaseKeys: showcaseKeys,
-      child: Scaffold(
-        appBar: CustomAppBar.colorGeneratorPage(
-          centerTitle: false,
-          titleTextStyle: context.textTheme.headlineMedium,
-          actions: [
-            Showcase(
-              key: _firstKey,
-              description: "Change theme",
-              child: ThemeButton(onToggleTheme: _toggleTheme),
-            ),
-            Showcase(
-              key: _secondKey,
-              description: "Show modal that could start tooltip tour",
-              child: TooltipButton(
-                onStartTooltip: () => _showTooltipDialog(context, true),
-              ),
-            ),
-            Showcase(
-              key: _thirdKey,
-              description: "Reset background color",
-              child: ResetButton(onResetData: _resetColor),
-            ),
-          ],
+    return MultiBlocListener(
+      listeners: [
+        BlocListener<ColorGeneratorBloc, ColorGeneratorState>(
+          listener: (_, colorGenState) =>
+              _updateHistory(colorGenState.data.bgColor),
+          listenWhen: (prev, curr) =>
+              prev.data.backgroundColor != curr.data.backgroundColor,
         ),
-        body: SafeArea(
-          bottom: false,
-          child: GestureDetector(
-            onTap: _updateBgColorAndColorLabel,
-            child: MainContainer(
-              child: BlocBuilder<ColorGeneratorBloc, ColorGeneratorState>(
-                builder: (context, state) {
-                  final bgColor = state.data.bgColor;
+      ],
+      child: ToolTips(
+        onComplete: (_, __) => _onCompleteGuideTour(),
+        onDismiss: (_) => _onCompleteGuideTour(),
+        autoStart: _tooltipsAutoStart,
+        showcaseKeys: showcaseKeys,
+        child: Scaffold(
+          appBar: CustomAppBar.colorGeneratorPage(
+            centerTitle: false,
+            titleTextStyle: context.textTheme.headlineMedium,
+            actions: [
+              Showcase(
+                key: _firstKey,
+                description: "Change theme",
+                child: ThemeButton(onToggleTheme: _toggleTheme),
+              ),
+              Showcase(
+                key: _secondKey,
+                description: "Show modal that could start tooltip tour",
+                child: TooltipButton(
+                  onStartTooltip: () => _showTooltipDialog(context, true),
+                ),
+              ),
+              Showcase(
+                key: _thirdKey,
+                description: "Reset background color",
+                child: ResetButton(onResetData: _resetColor),
+              ),
+            ],
+          ),
+          body: SafeArea(
+            bottom: false,
+            child: GestureDetector(
+              onTap: _updateBgColorAndColorLabel,
+              child: MainContainer(
+                child: BlocBuilder<ColorGeneratorBloc, ColorGeneratorState>(
+                  builder: (context, state) {
+                    final bgColor = state.data.bgColor;
 
-                  Color textColor = context.isDarkTextColor(bgColor)
-                      ? AppColors.white
-                      : AppColors.graphite;
+                    Color textColor = context.isDarkTextColor(bgColor)
+                        ? AppColors.white
+                        : AppColors.graphite;
 
-                  return Container(
-                    color: bgColor,
-                    child: Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Hello there!',
-                            style: context.textTheme.headlineMedium?.copyWith(
-                              color: textColor,
+                    return Container(
+                      color: bgColor,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Hello there!',
+                              style: context.textTheme.headlineMedium?.copyWith(
+                                color: textColor,
+                              ),
                             ),
-                          ),
-                          const SizedBox(height: 16.0),
-                          ColorInfo(
-                            keyForText: _fourthKey,
-                            keyForSlider: _fifthKey,
-                            colorLabel: state.data.label,
-                            textColor: textColor,
-                            opacity: state.data.opacityValue,
-                            onUpdateOpacity: _updateOpacity,
-                          ),
-                        ],
+                            const SizedBox(height: 16.0),
+                            ColorInfo(
+                              keyForText: _fourthKey,
+                              keyForSlider: _fifthKey,
+                              colorLabel: state.data.label,
+                              textColor: textColor,
+                              opacity: state.data.opacityValue,
+                              onUpdateOpacity: _updateOpacity,
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             ),
           ),
